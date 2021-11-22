3b738f9ea6467946a8334ad6efc820edc61fd732
hi9105 <88196723+hi9105@users.noreply.github.com>
2021-11-22 11:08:06 +0100

    added parameter " if_exists='replace' " to data frame saving as SQL

--- data/process_data.py
+++ data/process_data.py
@@ -57,7 +57,7 @@ def clean_data(df):
 def save_data(df, database_filename):
 
 				engine = create_engine('sqlite:///' + database_filename)
-				df.to_sql('EtlTable', engine, index=False)
+				df.to_sql('EtlTable', engine, index=False, if_exists='replace')
 
 
 def main():
